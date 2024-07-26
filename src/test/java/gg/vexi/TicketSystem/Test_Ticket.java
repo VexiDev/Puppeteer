0cc9d56e23d7583a61a48905dcbf732afb011d62
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-07-26 13:20:22 -0700

    Added ticket priority and ticket parameters to Ticket init test

--- src/test/java/gg/vexi/TicketSystem/Test_Ticket.java
+++ src/test/java/gg/vexi/TicketSystem/Test_Ticket.java
@@ -5,8 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.google.gson.JsonObject;
+
+import static gg.vexi.TicketSystem.TestUtils.assertJsonObjectEquals;
 import gg.vexi.TicketSystem.Ticket.ActionType;
 import gg.vexi.TicketSystem.Ticket.Ticket;
+import gg.vexi.TicketSystem.Ticket.TicketPriority;
 
 public class Test_Ticket {
 
@@ -14,7 +18,9 @@ public class Test_Ticket {
 
     @BeforeEach
     public void setup() {
-        Ticket = new Ticket(ActionType.ACTION);
+        JsonObject parameters = new JsonObject();
+        parameters.addProperty("setting_example", true);
+        Ticket = new Ticket(ActionType.ACTION, TicketPriority.NORMAL, parameters);
     }
 
     @Test
@@ -25,6 +31,17 @@ public class Test_Ticket {
         // verify type
         assertNotNull(Ticket.getType(), "Ticket has no action type");
         assertEquals(true, Ticket.getType() instanceof ActionType, "Ticket does not have a valid action type");
+
+        // verify priority
+        assertNotNull(Ticket.getPriority(), "Ticket has no priority level");
+        assertEquals(true, Ticket.getPriority() instanceof TicketPriority, "Ticket does not have a valid priority level");
+
+        // verify parameters
+        JsonObject expected_parameters = new JsonObject();
+        expected_parameters.addProperty("setting_example", true);
+
+        assertNotNull(Ticket.getParameters(), "Ticket has no action type");
+        assertJsonObjectEquals(expected_parameters, Ticket.getParameters());
 
     }
     
