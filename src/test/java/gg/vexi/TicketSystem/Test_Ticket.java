d67201d434b0c3730fcb3b9791a9a8cd76d3bb1e
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-07-26 10:44:51 -0700

    Added test to verify ticket has a valid actiontype

--- src/test/java/gg/vexi/TicketSystem/Test_Ticket.java
+++ src/test/java/gg/vexi/TicketSystem/Test_Ticket.java
@@ -1,16 +1,27 @@
 package gg.vexi.TicketSystem;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import gg.vexi.TicketSystem.Ticket.Ticket;
 
 public class Test_Ticket {
 
-    @Test
-    public void test_init() {
-        Ticket Ticket = new Ticket();
-        assertNotNull(Ticket, "Ticket is Null");
+    Ticket Ticket;
+
+    @BeforeEach
+    public void setup() {
+        Ticket = new Ticket();
     }
 
+    @Test
+    public void test_init() {
+        assertNotNull(Ticket, "Ticket is Null");
+        assertNotNull(Ticket.getType(), "Ticket has no action type");
+        assertEquals(true, Ticket.getType() instanceof ActionType, "Ticket does not have a valid action type");
+    }
+    
 }
