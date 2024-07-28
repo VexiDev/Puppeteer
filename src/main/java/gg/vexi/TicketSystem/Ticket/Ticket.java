c24e180629269960b9b68251ce705132d56c321a
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-07-28 14:37:31 -0700

    Implemented ID attribute into Ticket to pass tests

--- src/main/java/gg/vexi/TicketSystem/Ticket/Ticket.java
+++ src/main/java/gg/vexi/TicketSystem/Ticket/Ticket.java
@@ -1,21 +1,37 @@
 package gg.vexi.TicketSystem.Ticket;
 
+import java.util.UUID;
+
 import com.google.gson.JsonObject;
 
 public class Ticket {
 
+    private final UUID Id;
     private final ActionType type;
     private final TicketPriority priority;
     private final JsonObject parameters;
 
     public Ticket(ActionType ActionType, TicketPriority TicketPriority, JsonObject Parameters) {
+        Id = UUID.randomUUID();
         type = ActionType;
         priority = TicketPriority;
         parameters = Parameters;
     }
 
-    public ActionType getType() { return type; }
-    public TicketPriority getPriority() { return priority; }
-    public JsonObject getParameters() { return parameters; }
+    public ActionType getType() {
+        return type;
+    }
+
+    public TicketPriority getPriority() {
+        return priority;
+    }
+
+    public JsonObject getParameters() {
+        return parameters;
+    }
+
+    public UUID getId() {
+        return Id;
+    }
 
 }
