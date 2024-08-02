2774df804578b4844d88448f98240d458176f987
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-08-02 15:53:10 -0700

    Pass new CaughtExceptions Test: - Created CaughtExceptions class with Errors concurrent hashmap

---
+++ src/main/java/gg/vexi/TicketSystem/Exceptions/CaughtExceptions.java
@@ -0,0 +1,9 @@
+package gg.vexi.TicketSystem.Exceptions;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CaughtExceptions {
+
+    ConcurrentHashMap<Long, Error> Errors = new ConcurrentHashMap<>();
+
+}
