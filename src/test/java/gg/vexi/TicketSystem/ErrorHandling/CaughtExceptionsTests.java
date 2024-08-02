7e122f58506d9a9d2f62b25cc3828be846297ddb
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-08-02 16:18:15 -0700

    Errors is now an arraylist

--- src/test/java/gg/vexi/TicketSystem/ErrorHandling/CaughtExceptionsTests.java
+++ src/test/java/gg/vexi/TicketSystem/ErrorHandling/CaughtExceptionsTests.java
@@ -1,7 +1,7 @@
 package gg.vexi.TicketSystem.ErrorHandling;
 
 import java.lang.reflect.Field;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,12 +26,12 @@ class CaughtExceptionsTests {
         // get errors map from caughtexceptions using reflection
         Field CaughtExceptions_field = CaughtExceptions.getClass().getDeclaredField("Errors");
         CaughtExceptions_field.setAccessible(true);
-        ConcurrentHashMap<Long, Error> errors_map 
-        = (ConcurrentHashMap<Long, Error>) CaughtExceptions_field.get(CaughtExceptions);
+        ArrayList<Error> errors_list
+        = (ArrayList<Error>) CaughtExceptions_field.get(CaughtExceptions);
 
         //verify errors is an empty map and is not null
-        assertNotNull(errors_map, "Errors map is null");
-        assertEquals(new ConcurrentHashMap<>().size(), errors_map.size(), "Errors map is not an empty hashmap");
+        assertNotNull(errors_list, "Errors map is null");
+        assertEquals(0, errors_list.size(), "Errors map is not an empty hashmap");
 
     }
 
