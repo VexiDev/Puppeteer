26fe6f4d6381067d889339ff226dab8197af832b
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-08-02 18:48:27 -0700

    Made new TicketResult test pass: - Added getStatus() method - Added getExceptions() method - Added status attribute
    Missing:
    - TicketStatus enum
    - CaughtException init in the TicketResult constructor (which should receive an errors list)
    - Data being passed to the result

--- src/main/java/gg/vexi/TicketSystem/Ticket/TicketResult.java
+++ src/main/java/gg/vexi/TicketSystem/Ticket/TicketResult.java
@@ -2,13 +2,19 @@ package gg.vexi.TicketSystem.Ticket;
 
 import com.google.gson.JsonObject;
 
+import gg.vexi.TicketSystem.Exceptions.CaughtExceptions;
+
 public class TicketResult {
 
     private final Ticket target_ticket;
     private final JsonObject data = new JsonObject();
+    private final boolean status = true;
 
     public TicketResult(Ticket ticket) { target_ticket = ticket; }
 
     public Ticket getTicket() { return target_ticket; }
     public JsonObject getData() { return data; }
+
+    public boolean getStatus() { return status; }
+    public CaughtExceptions getExceptions() { return new CaughtExceptions(); }
 }
