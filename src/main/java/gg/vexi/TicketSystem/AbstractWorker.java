16a6f6938aa649f1074a8a43dd4d85a3f64eb15d
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-08-04 20:38:50 -0700

    Implemented CaughtExceptions into AbstractWorker: - Added attribute exceptionHandler() - Added recordException helper method   - just does CaughtExceptions.add() - Implemented start() method as the safe starting point of the worker to handle any unhandled exceptions - Renamed entry point method: start() -> main() - Moved order of method definitions for clarity - Renamed completeSuccess() -> complete() which now takes the status to exit with

--- src/main/java/gg/vexi/TicketSystem/AbstractWorker.java
+++ src/main/java/gg/vexi/TicketSystem/AbstractWorker.java
@@ -3,6 +3,7 @@ package gg.vexi.TicketSystem;
 import java.util.concurrent.CompletableFuture;
 
 import gg.vexi.TicketSystem.Exceptions.CaughtExceptions;
+import gg.vexi.TicketSystem.Exceptions.ExceptionRecord;
 import gg.vexi.TicketSystem.Ticket.Ticket;
 import gg.vexi.TicketSystem.Ticket.TicketResult;
 
@@ -11,30 +12,57 @@ public abstract class AbstractWorker<T> {
     Status status = Status.CREATED;
     final CompletableFuture<TicketResult<T>> future;
     final Ticket associated_ticket;
+    final CaughtExceptions exceptionHandler;
 
     public AbstractWorker(Ticket ticket) {
-        
-        future = new CompletableFuture<>();
+
         associated_ticket = ticket;
+        exceptionHandler = new CaughtExceptions();
+        future = new CompletableFuture<>();
         status = Status.READY;
 
     }
+    
+    // entry point to worker
+    public abstract void main();
+
+
+    // start point of worker (run by ticketmanager)
+    public final void start() {
+        try {
+            main();
+        } catch (Exception e) {
+            recordException(new ExceptionRecord("Unhandled Exception", e.getMessage()));
+            complete(Status.FAILED, null);
+        }
+    }
+
+    public final void recordException(ExceptionRecord record) { exceptionHandler.add(record); }
+
+    // exit point of worker
+    protected void complete(Status result_status, T data) {
+        TicketResult<T> result = new TicketResult<>(new CaughtExceptions(), associated_ticket, result_status, data);
+        future.complete(result);
+    }
+
+
 
     // getters
-    public Status getStatus() { return status; }
-    public CompletableFuture<TicketResult<T>> getFuture() { return future; }
-    public Ticket getTicket() { return associated_ticket; }
+    public Status getStatus() {
+        return status;
+    }
+
+    public CompletableFuture<TicketResult<T>> getFuture() {
+        return future;
+    }
+
+    public Ticket getTicket() {
+        return associated_ticket;
+    }
 
     // setters
-    public void setStatus(Status new_status) { status = new_status; }
-
-    public abstract void start();
-
-    public final void completeSuccess(T data) {
-
-        TicketResult<T> result = new TicketResult<>(new CaughtExceptions(), associated_ticket, Status.SUCCESS, data);
-        future.complete(result);
-        
+    public void setStatus(Status new_status) {
+        status = new_status;
     }
 
 }
