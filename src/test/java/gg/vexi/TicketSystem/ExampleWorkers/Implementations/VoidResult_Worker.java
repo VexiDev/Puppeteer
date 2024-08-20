489c9586d347778b1128787ba40c718357a1d288
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-08-19 18:17:01 -0700

    Implemented actual behavior into VoidResult_Worker to pass test_complete() test

--- src/test/java/gg/vexi/TicketSystem/ExampleWorkers/Implementations/VoidResult_Worker.java
+++ src/test/java/gg/vexi/TicketSystem/ExampleWorkers/Implementations/VoidResult_Worker.java
@@ -1,5 +1,6 @@
 package gg.vexi.TicketSystem.ExampleWorkers.Implementations;
 
+import gg.vexi.TicketSystem.Exceptions.ExceptionRecord;
 import gg.vexi.TicketSystem.Status;
 import gg.vexi.TicketSystem.annotations.AssociatedActionType;
 import gg.vexi.TicketSystem.core.AbstractWorker;
@@ -8,6 +9,8 @@ import gg.vexi.TicketSystem.ticket.Ticket;
 @AssociatedActionType("VoidResult_Worker")
 public class VoidResult_Worker extends AbstractWorker {
 
+    private Object data;
+
     public VoidResult_Worker(Ticket ticket) {
         super(ticket);
     }
@@ -15,5 +18,13 @@ public class VoidResult_Worker extends AbstractWorker {
     @Override
     public void main() {
         super.setStatus(Status.PROCESSING);
+
+        try {
+            Thread.sleep(200);
+            data = null;
+            super.complete(Status.SUCCESS, data);
+        } catch (InterruptedException e) {
+            super.recordException(new ExceptionRecord("InterruptedException", e.getMessage()));
+        }
     }
 }
