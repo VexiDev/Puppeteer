dfa97a0ce29e6b6066af74e45d3f0b524fa2ad91
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-09-01 23:14:04 -0400

    DEBUG STATE OF PUPPETEER MASTER CLASS ->
     - Implemented printDebug() and debug attribute
     - Added debug prints throughout puppeteer to track puppeteer steps and queue state throughout processing a ticket
     - Added 'synchronized'  modifier to all exposed methods ensuring concurrent safe execution of these methods
     - Moved Map initialization to constructor for better control over when these maps are defined
    
     - THIS STATE OF THE PUPPETEER MASTER CLASS IS AN INTERM STATE THAT WILL BE CLEANED UP IN THE FUTURE!

--- src/main/java/gg/vexi/Puppeteer/Puppeteer.java
+++ src/main/java/gg/vexi/Puppeteer/Puppeteer.java
@@ -20,13 +20,25 @@ import gg.vexi.Puppeteer.annotations.Scanner.AnnotationScanner;
 public class Puppeteer {
 
     private final PuppetRegistry puppetRegistry = new PuppetRegistry();
-    private final Map<String, PriorityBlockingQueue<Ticket>> actionQueues = new ConcurrentHashMap<>();
-    private final Map<String, Ticket> activeTickets = new ConcurrentHashMap<>();
+    private final Map<String, PriorityBlockingQueue<Ticket>> actionQueues;
+    private final Map<String, Ticket> activeTickets;
 
+    private final boolean debug;
 
     // auto registration should be optional and the registerPuppet() method should be public
-    public Puppeteer(String puppetPackage) {
+    public Puppeteer(String puppetPackage) { this(puppetPackage, false); }
+    public Puppeteer(String puppetPackage, boolean send_debug) {
+        printDebug("Initializing puppeteer!");
+        actionQueues = new ConcurrentHashMap<>();
+        activeTickets = new ConcurrentHashMap<>();
         autoRegisterPuppets(puppetPackage);
+        debug = send_debug;
+    }
+
+    public final void printDebug(String msg) {
+        if (debug) {
+            System.out.println(msg);
+        }
     }
     
     
@@ -45,33 +57,43 @@ public class Puppeteer {
     
     
     // overload if customer has not already created the ticket themselves with createTicket()
-    public Ticket queueTicket(String action_type, TicketPriority ticket_priority, JsonObject ticket_parameters) {
+    public synchronized Ticket queueTicket(String action_type, TicketPriority ticket_priority, JsonObject ticket_parameters) {
         Ticket ticket = createTicket(action_type, ticket_priority, ticket_parameters);
         queueTicket(ticket);
         return ticket;
     }
-    public void queueTicket(Ticket ticket) {
+    public synchronized void queueTicket(Ticket ticket) {
         addTicketToQueue(ticket);
         tryExecuteNextTicket(ticket.getType());   
     }
     
     protected final void addTicketToQueue(Ticket ticket) {
+        printDebug("Adding a ticket to queue "+ticket.getType()+" [Queue length is currently "+actionQueues.get(ticket.getType()).size()+"]");
         // this should handle key not found exceptions and return an error to the ticket future!
         actionQueues.get(ticket.getType()).offer(ticket);
+        printDebug("Queue size is now "+actionQueues.get(ticket.getType()).size());
     }
     
 
 
 
 
-
+    private synchronized Ticket pollForTicket(String type) { 
+        // printDebug("Polling for ticket of type "+type);
+        return actionQueues.get(type).poll(); 
+    }
 
     protected final Ticket nextTicket(String type) {
 
-        if (getActive(type) == null) {
-            return actionQueues.get(type).poll();
-
-        } else { return null; }
+        if (!isActive(type)) {
+            // printDebug("Queue size when polling for type "+type+": "+actionQueues.get(type).size());
+            Ticket next_ticket = pollForTicket(type);
+            printDebug("Polling... [Queue size is now "+actionQueues.get(type).size()+"]");
+            return next_ticket;
+            
+        } else { 
+            printDebug("A ticket is already being processed for type "+type);
+            return null; }
     }
 
 
@@ -79,14 +101,14 @@ public class Puppeteer {
 
         Ticket next_ticket = nextTicket(action_type);
 
-        //this should be handled and return an error to the ticket future!
-        if (next_ticket == null) { return; } 
+        if (next_ticket == null) { 
+            printDebug("nextTicket for type "+action_type+" returned null! (a ticket might be active or the queue is empty)");
+            return;
+        } 
+
 
         executeTicket(next_ticket);
     }
-
-
-
 
     protected final void executeTicket(Ticket ticket) {
 
@@ -95,10 +117,14 @@ public class Puppeteer {
         
         // initialise the relevant puppet and get the puppet future
         AbstractPuppet puppet = puppetRegistry.getPuppet(ticket);
-        CompletableFuture<TicketResult> puppetFuture = puppet.getFuture();
 
+        CompletableFuture<TicketResult> puppetFuture = puppet.getFuture();
+        
         // begin performance
+        // printDebug("Queue size before starting ticket ("+ticket.getType()+"): "+actionQueues.get(ticket.getType()).size());
         CompletableFuture.runAsync(() -> puppet.start());
+        // printDebug("Processing a ticket for type "+ticket.getType()+" | args: "+ticket.getParameters().toString());
+        // printDebug("Queue size after starting ticket ("+ticket.getType()+"): "+actionQueues.get(ticket.getType()).size());
         
         // pass result to ticket future when compelte (we will do more stuff here later)
         puppetFuture.thenAccept(result -> {
@@ -111,9 +137,10 @@ public class Puppeteer {
     protected final void completeTicket(Ticket ticket, TicketResult result) {
         
         ticket.getFuture().complete(result);
-
+        
         activeTickets.remove(ticket.getType());
-    
+        
+        printDebug(System.currentTimeMillis()+" - A Ticket of type "+ticket.getType()+" has completed! Attempting to execute next ticket!");
         tryExecuteNextTicket(ticket.getType());
     }
 
@@ -139,16 +166,18 @@ public class Puppeteer {
             String actionType = annotation.value().toLowerCase();
             if (actionType.isEmpty()) { actionType = puppetClass.getSimpleName().toLowerCase(); }
 
-            // add puppet to registry and create a queue for it
-            registerPuppet(puppetClass, actionType);
+            // add puppet to registry
+            registerPuppetConstructor(puppetClass, actionType);
+
+            // create a queue for the puppet
+            printDebug("Creating new queue for puppets with type -> "+actionType);
             actionQueues.put(actionType, new PriorityBlockingQueue<>());
         }
     }
 
-    private void registerPuppet(Class<?> puppetClass, String type) {
+    private void registerPuppetConstructor(Class<?> puppetClass, String type) {
         puppetRegistry.registerPuppet(type, (ticket) -> {
             try {
-                actionQueues.put(type, new PriorityBlockingQueue<>());
                 return (AbstractPuppet) puppetClass.getDeclaredConstructor(Ticket.class).newInstance(ticket);
             } catch (IllegalArgumentException | InstantiationException | NoSuchMethodException | SecurityException | InvocationTargetException | IllegalAccessException e) {
                 throw new RuntimeException("Failed to instantiate class", e);
@@ -158,24 +187,28 @@ public class Puppeteer {
 
 
     // getters
-    protected final PriorityBlockingQueue<Ticket> getQueue(String type) {
+    protected synchronized final PriorityBlockingQueue<Ticket> getQueue(String type) {
         return actionQueues.get(type);
     }
 
-    protected final Map<String, PriorityBlockingQueue<Ticket>> getAllQueues() {
+    protected synchronized final Map<String, PriorityBlockingQueue<Ticket>> getAllQueues() {
         return actionQueues;
     }
 
-    protected final Ticket getActive(String type) {
+    protected synchronized final Ticket getActive(String type) {
         return activeTickets.get(type);
     }
 
-    protected final Map<String, Ticket> getAllActive() {
+    protected synchronized final boolean isActive(String type) {
+        return activeTickets.containsKey(type);
+    }
+
+    protected synchronized final Map<String, Ticket> getAllActive() {
         return activeTickets;
     }
 
     // setters
-    protected final void setActive(Ticket ticket) {
+    protected synchronized final void setActive(Ticket ticket) {
         activeTickets.put(ticket.getType(), ticket);
     }
 
