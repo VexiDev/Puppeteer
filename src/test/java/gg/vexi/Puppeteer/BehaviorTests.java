67ad00b50dd2993ebc88b7fce85cb933b5e1d392
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-11-09 21:03:04 -0500

    Test changes:     - Improved comment explanations in behavior/integration tests         - These tests are still not finalized and may not test their           cases efficiently, 100% correctly or handle all edge cases     - General formatting improvements

--- src/test/java/gg/vexi/Puppeteer/BehaviorTests.java
+++ src/test/java/gg/vexi/Puppeteer/BehaviorTests.java
@@ -8,7 +8,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
@@ -91,9 +90,12 @@ class _Behavior {
     }
 
     @Test
-    public void ensureQueueIntegrity() {
+    public void test_queueIntegrity() {
         
         // This test is to ensure that queues correctly poll correctly and maintain queue sizing
+        // Note: 
+        //  - This test assumes an empty queue when it executes! If the queue is not empty it will fail!
+        //  - This test was written in a sleep deprived state a may not accurately test for queue integrity failure!
         
         // create ticket arguments
         String ticket_type = "test_action";
@@ -109,19 +111,26 @@ class _Behavior {
         List<CompletableFuture<TicketResult>> futures = new ArrayList<>();
         AtomicInteger completedCount = new AtomicInteger(0);
 
-        // the following loop queues 5 puppets and ensures the queue size increases accordingly
+        // this loop queues puppets and ensures the queue size increases accordingly
         for (int i=0; i<=num_test_tickets-1;i++) {
             Ticket ticket = Puppeteer.createTicket(ticket_type, priority, parameters);
             tickets.add(ticket);
             futures.add(ticket.getFuture());
             Puppeteer.queueTicket(ticket);   
             int expected = Puppeteer.getQueue(ticket_type).size();
-            assertEquals(expected, i);
+            assertEquals(expected, i); // 0 on first loop works because the first ticket is immediately polled from the queue
         }
         
+        // this loop ensures that the queue decreases when a ticket ends (if it doesn't then we trigger a flag) 
+        //   - This flag based implementation is pretty bruteforce and should
+        //      be refactored such that we immediately fail the test when a mismatch is detected!
+        //   - Additionally since a queue integrity failure would cause our futures 
+        //      to never complete we need a timeout on the test
+        //     (we could have puppets be able to kill themselves or 
+        //     ticketmanager kills a puppet [heartbeat style implementatino])
         AtomicBoolean flag = new AtomicBoolean(false); 
         for (int i=0; i<=tickets.size()-1;i++) {
-            // wait for tickets to complete and ensure the queue is what we expect
+            // wait for tickets to complete and ensure the queue is the size we expect
             futures.get(i).thenRun(() -> {
                 int actual = Puppeteer.getQueue(ticket_type).size();
                 int totalCompleted = completedCount.incrementAndGet();
@@ -130,11 +139,11 @@ class _Behavior {
             });
         }
         assertTimeoutPreemptively(
-            Duration.ofMillis(futures.size()*202),
+            Duration.ofMillis(futures.size()*300), // 100ms overhead for each future
             () -> {
                 CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).join();
             },
-            "Queued tickets took to long to execute (likely means that we failed to execute all tickets tickets!)\nEnsure that ticket queues are not being cleared and all ticket futures complete eventually!\n"
+            "Queued tickets took to long to execute (likely means that we failed to execute all tickets!)\nEnsure that ticket queues are not being cleared and all ticket futures complete eventually!\n"
         );
         assertEquals(false, flag.get(), "Queue count mismatch during processing!");
     }
