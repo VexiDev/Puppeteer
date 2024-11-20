dca69e7874125db186a0ea5677ce033936aca74c
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-11-20 01:42:05 -0500

    feat: Ticket now takes in generic T - This allows us to know and enforce type safety onto Result and the   associated Puppet.     - Only a Ticket of the correct type can queue a Puppet of that type       since both the Ticket `puppet` and type must match     - Removes the need to cast the return value of `Result.data()` to the       expected type - Ticket now gets a `Instant timestamp` value on creation - Puppet is now comparable to other puppets of any type using both   TicketPriority and timestamp allowing Puppets of the same Priority to   sort given their creation time. - Tickets now have a `completeExceptionally()` method that takes in a   problemHandler to automatically build the relevant Result object and   complete the ticket's result.

--- src/main/java/gg/vexi/Puppeteer/Core/Ticket.java
+++ src/main/java/gg/vexi/Puppeteer/Core/Ticket.java
@@ -1,63 +1,82 @@
 package gg.vexi.Puppeteer.Core;
 
+import java.time.Instant;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import gg.vexi.Puppeteer.Ticket.TicketPriority;
+import gg.vexi.Puppeteer.ResultStatus;
+import gg.vexi.Puppeteer.Exceptions.ProblemHandler;
 import gg.vexi.Puppeteer.Ticket.Result;
 
-public class Ticket implements Comparable<Ticket> {
+public class Ticket<T> implements Comparable<Ticket<?>> {
 
     private final UUID id;
+    private final Instant timestamp;
     private final String puppet;
     private final TicketPriority priority;
     private final Map<String, Object> parameters;
-    private final CompletableFuture<Result> future;
+    private final CompletableFuture<Result<T>> future;
+    
+    //TODO: Add overloads
 
     public Ticket(
-                String puppet,
-                TicketPriority priority, 
-                Map<String, Object> parameters, 
-                CompletableFuture<Result> future) 
-    {   
-        // arguemnts are all required 
+        String puppet,
+        TicketPriority priority,
+        // TODO: Convert parameters to be generic instead of Object
+        Map<String, Object> parameters,
+        CompletableFuture<Result<T>> future) {
+        // arguemnts are all required
         Objects.requireNonNull(puppet, "Ticket puppet cannot be null");
         Objects.requireNonNull(priority, "Ticket priority cannot be null");
         Objects.requireNonNull(parameters, "Ticket parameters cannot be null");
         Objects.requireNonNull(future, "Ticket future cannot be null");
 
         id = UUID.randomUUID();
+        timestamp = Instant.now();
         this.puppet = puppet.toLowerCase();
         this.priority = priority;
         this.parameters = parameters;
         this.future = future;
     }
 
+    // Sorts from highest to lowest priority then from oldest to newest timestamp
+    // Allows for a consistent FIFO queue even for equal priorities
     @Override
-    public int compareTo(Ticket other) {
-        return Integer.compare(other.priority.ordinal(), this.priority.ordinal());
+    public int compareTo(Ticket<?> other) {
+        int p = Integer.compare(other.priority.ordinal(), this.priority.ordinal());
+        return (p != 0) ? p : this.timestamp().compareTo(other.timestamp);
     }
 
     public String puppet() {
-        return puppet;
+        return this.puppet;
     }
 
     public TicketPriority priority() {
-        return priority;
+        return this.priority;
     }
 
     public synchronized Map<String, Object> parameters() {
-        return parameters;
+        return this.parameters;
     }
 
     public UUID id() {
-        return id;
+        return this.id;
     }
 
-    public CompletableFuture<Result> future() {
-        return future;
+    public Instant timestamp() {
+        return this.timestamp;
+    }
+
+    public CompletableFuture<Result<T>> future() {
+        return this.future;
+    }
+
+    public final void completeExceptionally(ProblemHandler problemHandler) {
+        this.future.complete(
+            Result.complete(null, ResultStatus.ERROR_FAILED, problemHandler));
     }
 
 }
