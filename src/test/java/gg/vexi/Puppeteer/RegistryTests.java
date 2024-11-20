f7169a444e5072a3d007fed716443ab9d1af408d
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-11-20 01:02:25 -0500

    Created test structure for registry tests

---
+++ src/test/java/gg/vexi/Puppeteer/RegistryTests.java
@@ -0,0 +1,35 @@
+package gg.vexi.Puppeteer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class _Registry {
+    
+    Registry reg;
+
+    @BeforeEach
+    public void setup() {
+        reg = new Registry();
+    }
+
+    @Test
+    public void testInit() {
+
+    }
+
+    @Test
+    public void testRegister() {
+
+    }
+
+    @Test
+    public void testRetrieve() {
+
+    }
+
+    @Test
+    public void testContains() {
+
+    }
+
+}
