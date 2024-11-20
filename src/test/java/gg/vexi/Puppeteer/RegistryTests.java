2d63315a5a13cfd77c9d5805e7d7cd88922327dd
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-11-20 01:30:29 -0500

    Defined registry test setup     - Before each test create a fresh registry object and reflect its       registry map object

--- src/test/java/gg/vexi/Puppeteer/RegistryTests.java
+++ src/test/java/gg/vexi/Puppeteer/RegistryTests.java
@@ -1,19 +1,36 @@
 package gg.vexi.Puppeteer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import gg.vexi.Puppeteer.Core.Puppet;
+import gg.vexi.Puppeteer.Core.Ticket;
+import gg.vexi.Puppeteer.ExamplePuppets.ExamplePuppet_String;
+import gg.vexi.Puppeteer.Ticket.TicketPriority;
+
+//TODO: Happy/Unhappy paths
 
 class _Registry {
-    
+
     Registry reg;
+    Map<String, Function<Ticket<?>, Puppet<?>>> reg_map;
 
     @BeforeEach
-    public void setup() {
+    @SuppressWarnings("unchecked")
+    public void setup() throws NoSuchFieldException, IllegalAccessException {
         reg = new Registry();
-    }
 
-    @Test
-    public void testInit() {
+        // get the registry map field reflection
+        Field reg_map_field = reg.getClass().getDeclaredField("registry");
+        reg_map_field.setAccessible(true);
+        reg_map = (Map<String, Function<Ticket<?>, Puppet<?>>>) reg_map_field.get(reg);
 
     }
 
